f19d601ba859fcd09dd99870a3154040f828ad11
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-06-22 16:20:41 -0300

    New Degug make compare

--- scripts/compare.py
+++ scripts/compare.py
@@ -5,9 +5,11 @@ from frictionless import Package
 def compare_data_resource_paths():
   data_resources_names = os.listdir('data')
   data_resources_paths = [os.path.join('data', i) for i in data_resources_names]
+  data_resources_paths.sort()
   print(data_resources_paths)
   local_package = Package('datapackage.json')
   datapackage_resources_paths = [i["path"] for i in local_package.resources]
+  datapackage_resources_paths.sort()
   print(datapackage_resources_paths)
   if data_resources_paths != datapackage_resources_paths:
     print("Resources presentes no arquivo datapackage.json diferente dos listados da pasta data.")
